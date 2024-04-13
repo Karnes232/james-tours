369276e1f5d47b45e0a1bedc2dcb9597ef43d61d
James Karnes <karnes.james@gmail.com>
2024-04-13 16:14:14 -0400

    playing with fixed for mobile

--- src/components/HeroImageComponent/HeroImageComponent.js
+++ src/components/HeroImageComponent/HeroImageComponent.js
@@ -24,6 +24,12 @@ const HeroImageComponent = ({ image }) => {
   return (
     <>
       <div className="w-full h-[50vh] lg:h-[80vh] xl:h-screen">
+        {/* <div
+          className={`h-full bg-center bg-no-repeat bg-cover lg:bg-fixed`}
+          style={HeroStyles}
+        >
+            
+        </div> */}
         <BackgroundImage
           placeholder={imageSrc[0]?.imageSrc}
           src={imageSrc[2]?.imageSrc}
