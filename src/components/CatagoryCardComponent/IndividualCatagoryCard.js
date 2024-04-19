20c093b21b1531ecf4a112270836247284924899
James Karnes <karnes.james@gmail.com>
2024-04-19 13:49:10 -0400

    added animation to category cards

--- src/components/CatagoryCardComponent/IndividualCatagoryCard.js
+++ src/components/CatagoryCardComponent/IndividualCatagoryCard.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
-
+import { motion } from "framer-motion";
 const IndividualCatagoryCard = ({ category }) => {
   console.log(category.node.url);
   const image = getImage(category.node.categoryImage.gatsbyImage);
@@ -9,16 +9,25 @@ const IndividualCatagoryCard = ({ category }) => {
     <>
       {/* <Link to={featured ? `tours/${url}` : `${url}`}> */}
       <Link to={`/${category.node.url}`}>
-        <div className="flex flex-col justify-center items-center max-w-[11rem] my-5 mx-2 rounded-lg overflow-hidden drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 1.5,
+            delay: 0.3,
+          }}
+          className="flex flex-col justify-center items-center max-w-[11rem] my-5 mx-2 rounded-lg  drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] "
+        >
           <GatsbyImage
             image={image}
             alt={category.node.categoryImage.title}
-            className="h-64 max-w-[12rem] rounded-[150px]"
+            className="h-64 max-w-[12rem] rounded-[150px] hover:scale-110 ease-in-out duration-300"
           />
           <div className="px-6 py-4 text-center max-w-[10rem] font-zeyada font-bold text-4xl leading-8">
             {category.node.category}
           </div>
-        </div>
+        </motion.div>
       </Link>
     </>
   );
