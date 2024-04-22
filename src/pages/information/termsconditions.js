9bef804c04d2576531f3630c4d384b98eaa224a5
James Karnes <karnes.james@gmail.com>
2024-04-22 18:24:40 -0400

    created cancellation, privacy & terms pages

--- src/pages/information/termsconditions.js
+++ src/pages/information/termsconditions.js
@@ -1,11 +1,33 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../../components/Layout/Layout";
+import HeroImageComponent from "../../components/HeroImageComponent/HeroImageComponent";
+import TextComponent from "../../components/TextComponent/TextComponent";
 const TermsConditions = ({ data }) => {
   return (
     <>
       <Layout layoutData={data.allContentfulLayout.nodes[0]}>
-        <div>termsconditions</div>{" "}
+        <HeroImageComponent
+          image={
+            data.allContentfulPageLayout.edges[0].node.pageHeroImage.gatsbyImage
+          }
+          effectImage={data.allContentfulAsset.edges[0].node.gatsbyImage}
+          title={data.allContentfulPageLayout.edges[0].node.title1}
+        />{" "}
+        <div className="xl:my-20">
+          <TextComponent
+            paragraph={
+              data.allContentfulPageLayout.edges[0].node.paragraph1.paragraph1
+            }
+            paragraphClassName="mb-4 lg:mb-0 xl:text-xl"
+          />
+          <TextComponent
+            paragraph={
+              data.allContentfulPageLayout.edges[0].node.paragraph2.paragraph2
+            }
+            paragraphClassName="mb-4 lg:mb-0 xl:text-xl"
+          />
+        </div>
       </Layout>
     </>
   );
@@ -23,6 +45,31 @@ export const query = graphql`
         tiktok
         logo {
           gatsbyImage(formats: WEBP, width: 400)
+        }
+      }
+    }
+    allContentfulPageLayout(filter: { pageName: { eq: "TermsConditions" } }) {
+      edges {
+        node {
+          title1
+          paragraph1 {
+            paragraph1
+          }
+          paragraph2 {
+            paragraph2
+          }
+          pageHeroImage {
+            gatsbyImage(placeholder: BLURRED, formats: WEBP, width: 3000)
+            title
+          }
+        }
+      }
+    }
+    allContentfulAsset(filter: { filename: { eq: "effect-shape.png" } }) {
+      edges {
+        node {
+          gatsbyImage(formats: WEBP, placeholder: BLURRED, width: 1500)
+          filename
         }
       }
     }
