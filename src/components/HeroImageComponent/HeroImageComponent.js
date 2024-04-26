c18a50ea23a849360773d759964048258f3645b6
James Karnes <karnes.james@gmail.com>
2024-04-26 18:45:41 -0400

    added animation on hero image text

--- src/components/HeroImageComponent/HeroImageComponent.js
+++ src/components/HeroImageComponent/HeroImageComponent.js
@@ -1,5 +1,6 @@
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
+import { motion } from "framer-motion";
 import BackgroundImage from "react-background-image";
 const HeroImageComponent = ({ image, effectImage, title, dark, short }) => {
   let heroImage = image?.images?.fallback?.srcSet.split(",");
@@ -31,9 +32,18 @@ const HeroImageComponent = ({ image, effectImage, title, dark, short }) => {
           <div
             className={`flex justify-center text-center items-center h-full w-screen !z-50 relative ${textPosition}`}
           >
-            <div className="font-zeyada text-6xl md:text-7xl lg:text-8xl md:w-96 lg:w-[26rem] text-primary-color">
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{
+                duration: 3,
+                delay: 0.3,
+              }}
+              className="font-zeyada text-6xl md:text-7xl lg:text-8xl md:w-96 lg:w-[26rem] text-primary-color"
+            >
               {title}
-            </div>
+            </motion.div>
           </div>
           <GatsbyImage
             image={imageEffect}
