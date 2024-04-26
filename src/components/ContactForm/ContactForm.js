ca6f61665725d5120881b5501d71d418cb184e84
James Karnes <karnes.james@gmail.com>
2024-04-26 17:29:20 -0400

    added animation to contact submit button

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ContactInfo from "./ContactInfo";
 import MoreInfo from "./MoreInfo";
-
+import { motion } from "framer-motion";
 const ContactForm = ({ formName, url }) => {
   const [name, setName] = useState("");
   return (
@@ -18,12 +18,35 @@ const ContactForm = ({ formName, url }) => {
         <input type="hidden" name="form-name" value={formName} />
         <ContactInfo name={name} setName={setName} />
         <MoreInfo />
-        <button
+        <motion.button
+          initial={{
+            opacity: 0.2,
+            scale: 0.5,
+            transition: {
+              duration: 3,
+              delay: 0.3,
+            },
+          }}
+          whileInView={{
+            opacity: 1,
+            scale: 0.9,
+            transition: {
+              duration: 3,
+              delay: 0.3,
+            },
+          }}
+          viewport={{ once: true }}
+          whileHover={{
+            scale: 1.1,
+            opacity: 0.7,
+            transition: { duration: 1 },
+          }}
+          whileTap={{ scale: 0.7, transition: { duration: 0.5 } }}
           type="submit"
-          className="w-32 lg:w-36 text-xs bg-black hover:opacity-75 text-white py-4 px-3 lg:py-5 lg:px-4 rounded-md uppercase font-bold"
+          className="w-32 lg:w-36 text-sm bg-black text-primary-color py-4 px-3 lg:py-5 lg:px-4 rounded-md uppercase font-bold"
         >
           Submit
-        </button>
+        </motion.button>
       </form>
     </>
   );
