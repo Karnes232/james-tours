17b9d151deaaffbd825aa19feb9d32ed3869eed5
James Karnes <karnes.james@gmail.com>
2024-04-26 14:20:49 -0400

    added z-index on navbar

--- src/components/Layout/HeaderComponents/Header.js
+++ src/components/Layout/HeaderComponents/Header.js
@@ -6,7 +6,7 @@ import HamburgerMenu from "./HamburgerMenu";
 const Header = ({ logo, layoutData }) => {
   return (
     <>
-      <nav className="bg-transparent w-screen z-50 h-24 md:h-36 fixed top-0 ">
+      <nav className="bg-transparent w-screen z-[100] h-24 md:h-36 fixed top-0 ">
         <div className="flex items-center justify-between bg-transparent max-w-6xl mx-5 md:mx-10 lg:mx-8 xl:mx-auto">
           <Logo logo={logo} />
           <LinksRight />
