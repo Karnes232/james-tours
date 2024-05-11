30cafe9d42ac8a68825ee30f3836b7ccc9ec9a20
James Karnes <karnes.james@gmail.com>
2024-05-11 18:46:45 -0400

    added text for weather

--- src/components/WeatherComponent/WeatherComponent.js
+++ src/components/WeatherComponent/WeatherComponent.js
@@ -4,7 +4,7 @@ import { RiCelsiusLine, RiFahrenheitLine } from "react-icons/ri";
 
 const WeatherComponent = ({ weather }) => {
   let time = useConvertTime(weather.last_updated.split(" ")[1]);
-  console.log(weather);
+  console.log(weather.condition.text);
   let tempatureC = weather.temp_c;
   let feelsLikeC = weather.feelslike_c;
   let tempatureF = weather.temp_f;
@@ -17,24 +17,27 @@ const WeatherComponent = ({ weather }) => {
           <div>{`${time.slice(0, -2)} ${time.slice(-2)}`}</div>
         </div>
         <div className="py-5 px-5 flex">
-          <div className="flex">
-            <img
-              src={`https:${weather.condition.icon}`}
-              alt={weather.condition.text}
-            />
-            <div className="flex flex-col">
-              <div className="flex justify-between items-center text-4xl font-bold space-x-1">
-                <div>{tempatureC.toFixed(0)}</div>
-                <div>
-                  <RiCelsiusLine size={17} />
+          <div className="flex flex-col">
+            <div className="flex">
+              <img
+                src={`https:${weather.condition.icon}`}
+                alt={weather.condition.text}
+              />
+              <div className="flex flex-col">
+                <div className="flex justify-between items-center text-4xl font-bold space-x-1">
+                  <div>{tempatureC.toFixed(0)}</div>
+                  <div>
+                    <RiCelsiusLine size={17} />
+                  </div>
+                </div>
+                <div className="flex justify-around items-center text-[.6rem] ">
+                  <span className="text-gray-500 tx">RealFeel</span>{" "}
+                  {feelsLikeC.toFixed(0)}
+                  <RiCelsiusLine size={7} />
                 </div>
               </div>
-              <div className="flex justify-around items-center text-[.6rem] ">
-                <span className="text-gray-500 tx">RealFeel</span>{" "}
-                {feelsLikeC.toFixed(0)}
-                <RiCelsiusLine size={7} />
-              </div>
             </div>
+            <div className="font-medium text-sm">{weather.condition.text}</div>
           </div>
           <div></div>
         </div>
