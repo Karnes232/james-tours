bd3128aa957f6f5bfdf54e13bf8f82562f4748b4
James Karnes <karnes.james@gmail.com>
2024-05-16 17:34:37 -0400

    added numbers to medical page

--- src/components/MedicalComponents/MedicalNumberComponent.js
+++ src/components/MedicalComponents/MedicalNumberComponent.js
@@ -1,24 +1,43 @@
 import React from "react";
 import TextComponent from "../TextComponent/TextComponent";
-import { list } from "postcss";
 
 const MedicalNumberComponent = ({ title, numbers }) => {
   let listedNumbers = [];
   numbers.split(/\r?\n/).forEach((number) => {
     let split = number.split(" ");
-    let hello = split[0].replace("-", " ");
-    split[0] = hello;
-    listedNumbers.push(split);
+    let replaced = split[0].replaceAll("-", " ");
+    split[0] = replaced;
+    let obj = {
+      name: split[0],
+      number: split[1],
+    };
+    listedNumbers.push(obj);
   });
 
-  console.log(listedNumbers);
   return (
     <>
       <div className="my-10 xl:my-15 mx-5">
         <TextComponent
           title={title}
-          titleClassName="my-5 2xl:mb-10 text-4xl md:text-6xl lg:text-8xl font-zeyada text-primary-color"
+          titleClassName="my-5 2xl:mb-10 text-4xl md:text-6xl lg:text-8xl font-zeyada text-primary-color lg:text-center"
         />
+        <div className="flex flex-col md:flex-row md:flex-wrap md:justify-evenly 2xl:space-x-10 mx-auto lg:mt-10">
+          <div className="max-w-sm min-w-[22rem] md:min-w-[30rem] my-5 mx-2 rounded-lg overflow-hidden shadow-lg flex flex-col bg-zinc-50 bg-opacity-60">
+            <div className="">
+              {listedNumbers.map((emergencyContact, index) => {
+                return (
+                  <div
+                    className="flex justify-between py-5 border-b text px-5"
+                    key={index}
+                  >
+                    <div>{emergencyContact.name}</div>
+                    <div> {emergencyContact.number}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
